d09533b7fe46e9a9c737f8fa3472693cc15efbab
Rafael Rodrigues <rodriguesrafael446@gmail.com>
2021-09-22 16:52:06 -0300

    Validação do formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,76 @@
-const dataInicial = document.getElementById("dataCriacao")
-const dataFinal = document.getElementById("dataLimite")
-const descricao = document.getElementById("descricao")
+// INPUTS
+const dataCriacao = document.getElementById("dataCriacao");
+const dataLimite = document.getElementById("dataLimite");
+const descricao = document.getElementById("descricao");
 
-var data = new Date();
-var dia = String(data.getDate()).padStart(2, '0');
-var mes = String(data.getMonth() + 1).padStart(2, '0');
-var ano = data.getFullYear();
-dataAtual = dia + '/' + mes + '/' + ano;
-console.log(dataAtual);
+dataLimite.addEventListener("blur", () => validarCampoVazio(dataLimite));
+descricao.addEventListener("blur", () => {
+  let campo = validarCampoVazio(descricao);
+  if(!campo) {
+    validarTamanhoCampo(descricao);
+  }
+});
 
-dataInicial.value = dataAtual
+function validarTamanhoCampo(input) {
+  let erros = false;
+  if(input.value.length <= 10) {
+    input.style.borderColor = "red";
+    input.nextElementSibling.innerHTML = "Este campo precisa ter mais de 10 caracteres";
+    erros = true;
+  } else {
+    input.style.borderColor = "green";
+    input.nextElementSibling.innerHTML = "";
+  }
 
+  return erros;
+}
+
+function validarCampoVazio(input) {
+  let erros = false;
+  if(input.value == "" || input.value === null) {
+    input.style.borderColor = "red";
+    input.nextElementSibling.innerHTML = "Por favor, preencha este campo!";
+    erros = true;
+  } else {
+    input.style.borderColor = "green";
+    input.nextElementSibling.innerHTML = "";
+  }
+
+  return erros;
+}
+
+function validarFormulario() {
+  let erros = false;
+  let inputs = document.getElementsByName("inputs-criar-card");
+  console.log(inputs);
+  
+  // VALIDAR O CAMPO VAZIO
+  for(input of inputs) {
+    let resposta = validarCampoVazio(input);
+    if(resposta) erros = true;
+  }
+
+  // VALIDAR O TAMANHO DO CAMPO
+  if(!erros) {
+    for(input of inputs) {
+      if(input.id == "descricao") {
+        let resposta = validarTamanhoCampo(input);
+        if(resposta) erros = true;
+      }
+    }
+  }
+
+  if(erros) return;
+  
+
+  // AQUI É SO CHAMAR A FUNÇÃO PARA CRIAR O CARD
+}
+
+function criarCard() {
+  alert("ME CHAMARAAAAAAAAAAAAAAM");
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validarFormulario();
+})
